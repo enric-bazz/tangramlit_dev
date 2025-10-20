6816232bc0542cfe0869b3744612969d5ef580a8
enric-bazz <enrico.bazzacco02@outlook.it>
2025-10-20 14:50:25 +0200

    test new graphics functions

--- test_script.py
+++ test_script.py
@@ -15,6 +15,9 @@ def train_mapper(data, config):
     adata_sc=data[0]
     adata_st=data[1]
 
+    if config['filter'] and (config['target_count'] == 'None'):
+        config['target_count'] = None  # remove string formatting
+
     adata_map, mapper, datamodule = tgl.map_cells_to_space(
         adata_sc=data[0],
         adata_st=data[1],
@@ -25,17 +28,25 @@ def train_mapper(data, config):
     ad_ge = tgl.project_sc_genes_onto_space(adata_map, datamodule)
     df = tgl.compare_spatial_gene_expr(ad_ge, datamodule)
     tgl.plot_training_scores(df)
-    tgl.plot_auc_curve(df)
+    # tgl.plot_auc_curve(df)  # works only with validation genes
 
     # Plot loss terms
-    tgl.plot_loss_term(adata_map=adata_map, loss_key='count_reg')
     tgl.plot_training_history(adata_map=adata_map, hyperpams=mapper.hparams, log_scale=False, lambda_scale=True)
+    
+    tgl.plot_loss_terms(adata_map=adata_map, loss_key=['count_reg', 'main_loss', 'ct_island_term'], lambda_coeff=[1.0e-3, 1, 1])
+    tgl.plot_loss_terms(adata_map=adata_map, loss_key=['count_reg', 'main_loss', 'ct_island_term'], lambda_coeff=[1.0e-3, 1, 1],
+                                                       lambda_scale=True, make_subplot=True, subplot_shape=(1,3))
+
+    # Plot final filter values distribution
+    if config["filter"]:
+        tgl.plot_filter_weights(adata_map, plot_heatmap=False, plot_spaghetti=True, plot_envelope=True)
+        tgl.plot_filter_count(adata_map, target_count=config['target_count'])
+        filt_corr = tgl.compute_filter_corr(adata_map, plot=True)
 
     # Try analysis functions
     df1 = tgl.get_cell_spot_pair(adata_map, filter=True)
     df2 = tgl.get_spot_cell_pair(adata_map, filter=True)
 
-    filt_corr = tgl.compute_filter_corr(adata_map, plot=True)
 
     # Shared labels
     labels = set(adata_st.obs['class_label']) & set(adata_sc.obs['class_label'])
@@ -45,16 +56,12 @@ def train_mapper(data, config):
                                 flavour='spot_to_cell', filter=False)
     acc, stats = tgl.annotation_report(true, pred)
 
-    map_sim = tgl.deterministic_mapping_similarity(adata_map, adata_sc, adata_st, flavour='spot_to_cell', filter=False)
+    map_sim = tgl.deterministic_mapping_similarity(adata_map, adata_sc, adata_st, flavour='spot_to_cell', filter=config['filter'])
 
-    # Plot final filter values distribution
-    # if mode == "filter":
-        # tgl.plot_filter_weights(adata_map, plot_spaghetti=True, plot_envelope=True)
-        # tgl.plot_filter_count(adata_map, target_count=target_count)
 
     # Annotation transfer test
-    # tg.transfer_annotation(adata_map=ad_map_lt, adata_st=adata_st, sc_cluster_label='class_label',
-    #                     filter=False)
+    tgl.transfer_annotation(adata_map=adata_map, adata_st=adata_st, sc_cluster_label='class_label',
+                        filter=config['filter'])
     
     return adata_map, mapper, datamodule
 
@@ -106,7 +113,6 @@ def train_validate_mapper(data, config, random_state=None):
     tgl.plot_training_scores(df)
     tgl.plot_auc_curve(df)
 
-
     return [adata_map, train_genes, val_genes, results], shared_genes, mapper, datamodule
 
 def cv_mapper_genes(data, config, genes_list):
@@ -125,17 +131,17 @@ def cv_mapper_genes(data, config, genes_list):
 
 def main():
 
-    path = "/nfsd/sysbiobig/bazzaccoen/tangramlit_dev/data"
-    # path = "C:/Users/enric/tangram/myDataCropped"
+    # path = "/nfsd/sysbiobig/bazzaccoen/tangramlit_dev/data"
+    path = "C:/Users/enric/tangram/myDataCropped"
 
     data = data_loading(path)
 
     with open("train_config.yaml", "r") as f:
         config = yaml.safe_load(f)
 
-    # train_mapper(data, config)
+    train_mapper(data, config)
 
-    _ , shared_genes, mapper, datamodule = train_validate_mapper(data, config)
+    # _ , shared_genes, mapper, datamodule = train_validate_mapper(data, config)
 
     # cv_mapper_genes(data, config, genes_list=shared_genes)
     
